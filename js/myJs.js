8e6a9d91191d97ba0dbd32d9344eb81cf2187778
Lengochuy13 <lengochuyc2130220002@gmail.com>
2022-01-17 10:19:19 +0700

    first commit

---
+++ js/myJs.js
@@ -0,0 +1,155 @@
+const textConfig = {
+  text1: "Xin chào cậu!",
+  text2: "Tớ là Lê Ngọc Huy, tớ có điều này muốn hỏi cậu, cậu phải trả lời thật lòng nha!!!",
+  text3: "Tớ muốn làm bạn với cậu có được không nào???",
+  text4: "Nếu cậu không trả lời mà thoát ra là tớ buồn lắm đó 😭",
+  text5: "Không được 😭",
+  text6: "Đồng ý 😍",
+  text7: "Hãy nói tớ nghe lí do cậu đồng ý đi :>>",
+  text8: "Gửi đến Lê Ngọc Huy",
+  text9: "Vì cậu rất hài hước và xứng đáng với điều đó.......",
+  text10: "Tớ biết mà 😝 Cám ơn cậu 3000",
+  text11: "Tối nay tớ sẽ nhắn tin cho cậu nhé 😄 Còn giờ thì chờ gì nữa mà không nhắn tin cho tớ đi nè!!!",
+  text12: "Chuyển đến Facebook :>",
+};
+
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    firstQuestion();
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 2000);
+
+  $("#text3").html(textConfig.text3);
+  $("#text4").html(textConfig.text4);
+  $("#no").html(textConfig.text5);
+  $("#yes").html(textConfig.text6);
+
+  function firstQuestion() {
+    $(".content").hide();
+    Swal.fire({
+      title: textConfig.text1,
+      text: textConfig.text2,
+      imageUrl: "img/xinchaoo.gif",
+      imageWidth: 300,
+      imageHeight: 300,
+      background: '#fff url("img/iput-bg.jpg")',
+      imageAlt: "Custom image",
+    }).then(function () {
+      $(".content").show(200);
+    });
+  }
+
+  // switch button position
+  function switchButton() {
+    var audio = new Audio("sound/duck.mp3");
+    audio.play();
+    var leftNo = $("#no").css("left");
+    var topNO = $("#no").css("top");
+    var leftY = $("#yes").css("left");
+    var topY = $("#yes").css("top");
+    $("#no").css("left", leftY);
+    $("#no").css("top", topY);
+    $("#yes").css("left", leftNo);
+    $("#yes").css("top", topNO);
+  }
+  // move random button póition
+  function moveButton() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    if (screen.width <= 600) {
+      var x = Math.random() * 300;
+      var y = Math.random() * 500;
+    } else {
+      var x = Math.random() * 500;
+      var y = Math.random() * 500;
+    }
+    var left = x + "px";
+    var top = y + "px";
+    $("#no").css("left", left);
+    $("#no").css("top", top);
+  }
+
+  var n = 0;
+  $("#no").mousemove(function () {
+    if (n < 1) switchButton();
+    if (n > 1) moveButton();
+    n++;
+  });
+  $("#no").click(() => {
+    if (screen.width >= 900) switchButton();
+  });
+
+  // generate text in input
+  function textGenerate() {
+    var n = "";
+    var text = " " + textConfig.text9;
+    var a = Array.from(text);
+    var textVal = $("#txtReason").val() ? $("#txtReason").val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+      for (let i = 1; i <= count; i++) {
+        n = n + a[i];
+        if (i == text.length + 1) {
+          $("#txtReason").val("");
+          n = "";
+          break;
+        }
+      }
+    }
+    $("#txtReason").val(n);
+  }
+
+  // show popup
+  $("#yes").click(function () {
+    var audio = new Audio("sound/tick.mp3");
+    audio.play();
+    Swal.fire({
+      title: textConfig.text7,
+      html: true,
+      width: 900,
+      padding: "3em",
+      html: "<input type='text' class='form-control' id='txtReason'  placeholder='Tại sao thế'>",
+      background: '#fff url("img/iput-bg.jpg")',
+      backdrop: `
+                    rgba(0,0,123,0.4)
+                    url("img/thankyou.gif")
+                    center
+                    no-repeat
+                  `,
+      showCancelButton: false,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonColor: "#fe8a71",
+      cancelButtonColor: "#f6cd61",
+      confirmButtonText: textConfig.text8,
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          width: 900,
+          confirmButtonText: textConfig.text12,
+          background: '#fff url("img/iput-bg.jpg")',
+          title: textConfig.text10,
+          text: textConfig.text11,
+          confirmButtonColor: "#83d0c9",
+          onClose: () => {
+            window.location = "https://www.facebook.com/profile.php?id=100019732021938";
+          },
+        });
+      }
+    });
+
+    $("#txtReason").focus(function () {
+      var handleWriteText = setInterval(function () {
+        textGenerate();
+      }, 10);
+      $("#txtReason").blur(function () {
+        clearInterval(handleWriteText);
+      });
+    });
+  });
+});
